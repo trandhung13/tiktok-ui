86f65ee87b825d6bf91abe16a4a2f4616215649e
Tran Duc Hung <trandhung1308@gmail.com>
2023-01-10 00:26:33 +0700

    Xây Dựng UI Phần Header  Tiktok #5

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -26,6 +26,21 @@ const MENU_ITEMS = [
   {
     icon: <FontAwesomeIcon icon={faEarthAmerica} />,
     title: "English",
+    children: {
+      title: "Language",
+      data: [
+        {
+          type: "language",
+          code: "en",
+          title: "English",
+        },
+        {
+          type: "language",
+          code: "vi",
+          title: "Tiếng Việt",
+        },
+      ],
+    },
   },
   {
     icon: <FontAwesomeIcon icon={faQuestionCircle} />,
@@ -46,6 +61,16 @@ function Header() {
       setSearchResult([]);
     }, 0);
   }, []);
+
+  // handle logic
+  const handleMenuChange = (menuItem) => {
+    switch (menuItem.type) {
+      case "language":
+        // handle change language
+        break;
+      default:
+    }
+  };
 
   return (
     <header className={cx("wrapper")}>
@@ -88,7 +113,7 @@ function Header() {
           </Button>
           <Button primary>Log in</Button>
 
-          <Menu items={MENU_ITEMS}>
+          <Menu items={MENU_ITEMS} onChange={handleMenuChange}>
             <button className={cx("more-btn")}>
               <FontAwesomeIcon icon={faEllipsisVertical} />
             </button>
